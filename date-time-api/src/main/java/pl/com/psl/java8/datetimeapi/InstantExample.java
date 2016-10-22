cfb62f58e2215cc85884a13ec007db288511dbe9
psl <pawel.slowik.dev@gmail.com>
2016-10-22 11:11:26 +0200

    Improved InstantExample

--- date-time-api/src/main/java/pl/com/psl/java8/datetimeapi/InstantExample.java
+++ date-time-api/src/main/java/pl/com/psl/java8/datetimeapi/InstantExample.java
@@ -24,9 +24,22 @@ public class InstantExample {
         LOGGER.info("Time travelled to Epoch!");
         LOGGER.info("Instant now:" + instantNow);
         LOGGER.info("Instant Epoch:" + instantEpoch);
-        LOGGER.info("Fast forward 10 seconds");
-        instantNow = instantNow.plus(10, ChronoUnit.SECONDS);
+        LOGGER.info("Fast forward 1 seconds, milli and nano");
+        instantNow = instantNow.plus(1, ChronoUnit.SECONDS).plusMillis(1).plusNanos(1);
         LOGGER.info("Instant now:" + instantNow);
         LOGGER.info("Seconds from Epoch:" + instantNow.getEpochSecond());
+        LOGGER.info("Fast forward 1 day");
+        instantNow = instantNow.plus(1, ChronoUnit.DAYS);
+        LOGGER.info("Instant now:" + instantNow);
+        try{
+            LOGGER.info("Try to fast forward 1 week...");
+            instantNow = instantNow.plus(1, ChronoUnit.WEEKS);
+        }
+        catch(Exception e){
+            LOGGER.error("Operation not allowed", e);
+        }
+        finally {
+            LOGGER.info("Instant now:" + instantNow);
+        }
     }
 }
